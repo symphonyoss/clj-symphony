09e3ee3a478f7f3d028e70cc4a203cc4d37e9b70
Peter Monks <pmonks@gmail.com>
2016-08-11 13:45:18 -0700

    :new: First commit

---
+++ project.clj
@@ -0,0 +1,32 @@
+;
+; Copyright © 2016 Symphony Software Foundation
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+; 
+;     http://www.apache.org/licenses/LICENSE-2.0
+; 
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+; 
+
+(defproject foundation.symphony/clj-symphony "0.1.0-SNAPSHOT"
+  :description        "A Clojure wrapper around the symphony-java-client library."
+  :url                "https://github.com/pmonks/clj-symphony"
+  :license            {:name "Apache License, Version 2.0"
+                       :url  "http://www.apache.org/licenses/LICENSE-2.0"}
+  :min-lein-version   "2.5.0"
+  :repositories       [["sonatype-snapshots" {:url "https://oss.sonatype.org/content/groups/public" :snapshots true}]]
+  :dependencies       [
+                        [org.clojure/clojure                      "1.8.0"]
+                        [org.symphonyoss.symphony/symphony-client "0.9.0-SNAPSHOT"]
+                      ]
+  :profiles           {:dev {:dependencies [[midje      "1.8.3"]]
+                             :plugins      [[lein-midje "3.2"]]}   ; Don't remove this or travis-ci will assplode!
+                       :uberjar {:aot :all}}
+  :lein-release       {:deploy-via :clojars}
+  )
---
+++ src/clj_symphony/api.clj
@@ -0,0 +1,84 @@
+;
+; Copyright © 2016 Symphony Software Foundation
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+; 
+;     http://www.apache.org/licenses/LICENSE-2.0
+; 
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+; 
+
+(ns clj-symphony.api)
+
+(defn- parse-params
+  "Parses connection parameters, substituting a :pod-id for the default URLs, if present."
+  [params]
+  (if-let [pod-id (:pod-id params)]
+    (dissoc (assoc params
+                   :session-auth-url (str "https://" pod-id "-api.symphony.com/sessionauth")
+                   :key-auth-url     (str "https://" pod-id "-api.symphony.com/keyauth")
+                   :agent-api-url    (str "https://" pod-id "-api.symphony.com/agent")
+                   :pod-api-url      (str "https://" pod-id "-api.symphony.com/pod"))
+            :pod-id)
+    params))
+
+(defn connect
+  "Connect to a Symphony pod as a given service account user.  Returns a 'session' object
+  that should be used in all subsequent API calls.
+
+  params is a map containing:
+  :pod-id           The id of the pod to connect to - will result the 4 URLs to be autopopulated (only appropriate for simple / business tier deployments).
+  :session-auth-url The URL of the session authentication endpoint.
+  :key-auth-url     The URL of the key authentication endpoint.
+  :agent-api-url    The URL of the agent API.
+  :pod-api-url      The URL of the Pod API.
+  :trust-store      A pair of strings containing the path to the trust store and the password of the trust store (mandatory).
+  :user-cert        A pair of strings containing the path to the bot user's certificate and the password of that certificate (mandatory).
+  :user-email       The email address of the bot user (mandatory).
+
+  Either :pod-id or (:session-auth-url and :key-auth-url and :agent-api-url and :pod-api-url) are mandatory."
+  [params]
+  (let [params           (parse-params params)
+        session-auth-url (:session-auth-url params)
+        key-auth-url     (:key-auth-url     params)
+        agent-api-url    (:agent-api-url    params)
+        pod-api-url      (:pod-api-url      params)
+        trust-store      (:trust-store      params)
+        user-cert        (:user-cert        params)
+        user-email       (:user-email       params)
+        auth-client      (org.symphonyoss.symphony.clients.AuthorizationClient. session-auth-url key-auth-url)
+        _                (.setKeystores auth-client (first trust-store) (second trust-store)
+                                                    (first user-cert)   (second user-cert))
+        auth             (.authenticate auth-client)
+        symph-client     (org.symphonyoss.client.SymphonyClientFactory/getClient org.symphonyoss.client.SymphonyClientFactory$TYPE/BASIC)
+        _                (.init symph-client auth user-email agent-api-url pod-api-url)]
+      symph-client))
+
+(defn- mapify
+  [x]
+  (dissoc (bean x) :class))
+
+(defn- user-obj
+  ([^org.symphonyoss.client.SymphonyClient session]               (.getLocalUser session))
+  ([^org.symphonyoss.client.SymphonyClient session ^Long user-id] (.getUserFromId (.getUsersClient session) user-id)))
+
+(defn user-info
+  "Returns a map containing information about the given user, or the authenticated session user if a user id is not provided."
+  ([^org.symphonyoss.client.SymphonyClient session]               (mapify (user-obj session)))
+  ([^org.symphonyoss.client.SymphonyClient session ^Long user-id] (mapify (user-obj session user-id))))
+
+(defn user-presence
+  "Returns the presence status of the given user, or all users."
+  ([^org.symphonyoss.client.SymphonyClient session]               (map mapify (.getAllUserPresence (.getPresenceClient session))))
+  ([^org.symphonyoss.client.SymphonyClient session ^Long user-id] (mapify (.getUserPresence (.getPresenceClient session) user-id))))
+
+(defn get-chats
+  "Returns a list of chats for the given user, or for the authenticated session user if a user id is not provided."
+  ([^org.symphonyoss.client.SymphonyClient session]               (map mapify (.getChats (.getChatService session) (user-obj session))))
+  ([^org.symphonyoss.client.SymphonyClient session ^Long user-id] (map mapify (.getChats (.getChatService session) (user-obj session user-id)))))
