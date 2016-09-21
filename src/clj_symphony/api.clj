d66002135873fe2c0950a5f8288f4061e4867e12
Peter Monks <pmonks@gmail.com>
2016-09-21 08:10:18 -0700

    :new: Add nil handling in user multimethod

--- src/clj_symphony/api.clj
+++ src/clj_symphony/api.clj
@@ -4,15 +4,15 @@
 ; Licensed under the Apache License, Version 2.0 (the "License");
 ; you may not use this file except in compliance with the License.
 ; You may obtain a copy of the License at
-; 
+;
 ;     http://www.apache.org/licenses/LICENSE-2.0
-; 
+;
 ; Unless required by applicable law or agreed to in writing, software
 ; distributed under the License is distributed on an "AS IS" BASIS,
 ; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
-; 
+;
 
 (ns clj-symphony.api)
 
@@ -78,6 +78,10 @@
 (defmethod user :current-user
   [^org.symphonyoss.client.SymphonyClient session]
   (.getLocalUser session))
+
+(defmethod user nil
+  [^org.symphonyoss.client.SymphonyClient session user-id]
+  nil)
 
 (defmethod user Long
   [^org.symphonyoss.client.SymphonyClient session ^Long user-id]
