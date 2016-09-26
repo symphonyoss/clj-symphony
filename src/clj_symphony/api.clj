8121368985f63560e36840cfb509890183b31614
Peter Monks <pmonks@gmail.com>
2016-09-25 20:52:47 -0700

    :new: Added message listening fns

--- src/clj_symphony/api.clj
+++ src/clj_symphony/api.clj
@@ -14,7 +14,8 @@
 ; limitations under the License.
 ;
 
-(ns clj-symphony.api)
+(ns clj-symphony.api
+  (:require [clojure.string :as s]))
 
 (defn- parse-params
   "Parses connection parameters, substituting a :pod-id for the default URLs, if present."
@@ -136,3 +137,39 @@
     (.sendMessage (.getMessageService session) chat msg)
     nil))
 
+(defn register-message-listener
+  "Registers f, a function of 7 parameters, as a message listener (callback), and returns a handle to that listener
+  so that it can be deregistered later on, if needed.  The 7 arguments passed to f are:
+
+     msg-id     - Identifier of the message
+     timestamp  - Timestamp the message was sent (as a string in ####???? format)
+     stream-id  - Identifier of the stream (chat or room) the message was sent to)
+     user-id    - Identifier of the user who sent the message
+     msg-format - Format of the message as a keyword (:messageml or :text)
+     msg-type   - ####????
+     msg        - Text of the message
+
+   The value returned by f (if any) is ignored."
+  [^org.symphonyoss.client.SymphonyClient session f]
+  (let [listener (reify
+                   org.symphonyoss.client.services.MessageListener
+                   (onMessage [this msg]
+                     (let [msg-t      ^org.symphonyoss.symphony.clients.model.SymMessage msg
+                           msg-id     (.getId          msg-t)
+                           timestamp  (.getTimestamp   msg-t)
+                           stream-id  (.getStreamId    msg-t)
+                           user-id    (.getFromUserId  msg-t)
+                           msg-format (when-not (nil? (.getFormat msg-t))
+                                        (keyword (s/lower-case (str (.getFormat msg-t)))))
+                           msg-type   (.getMessageType msg-t)
+                           msg-text   (.getMessage     msg-t)]
+                      (f msg-id timestamp stream-id user-id msg-format msg-type msg-text))))]
+    (.registerMessageListener (.getMessageService session) listener)
+    listener))
+
+(defn deregister-message-listener
+  "Deregisters a previously-registered message listener.  Once deregistered, a listener should be discarded.
+  Returns true if a valid message listener was deregistered, false otherwise."
+  [^org.symphonyoss.client.SymphonyClient session ^org.symphonyoss.client.services.MessageListener listener]
+  ; #### WARNING: https://github.com/symphonyoss/symphony-java-client/issues/19
+  (.removeMessageListener (.getMessageService session) listener))
