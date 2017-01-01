0503ae72d528b8e47e975930af44469e136fecf5
Peter Monks <pmonks@gmail.com>
2017-01-01 13:25:37 -0800

    :new: Fix issue #3

--- project.clj
+++ project.clj
@@ -14,20 +14,28 @@
 ; limitations under the License.
 ;
 
-(defproject org.symphonyoss.symphony/clj-symphony "0.1.0-SNAPSHOT"
-  :description        "A Clojure wrapper around the symphony-java-client library."
-  :url                "https://github.com/symphonyoss/clj-symphony"
-  :license            {:name "Apache License, Version 2.0"
-                       :url  "http://www.apache.org/licenses/LICENSE-2.0"}
-  :min-lein-version   "2.5.0"
-  :repositories       [["sonatype-snapshots" {:url "https://oss.sonatype.org/content/groups/public" :snapshots true}]]
-  :plugins            [[lein-licenses "0.2.1"]]
-  :dependencies       [
-                        [org.clojure/clojure                      "1.8.0"]
-                        [org.symphonyoss.symphony/symphony-client "1.0.0-SNAPSHOT"]
-                      ]
-  :profiles           {:dev {:dependencies [[midje      "1.8.3"]]
-                             :plugins      [[lein-midje "3.2"]]}   ; Don't remove this or travis-ci will assplode!
-                       :uberjar {:aot :all}}
-  :lein-release       {:deploy-via :clojars}
+(defproject org.symphonyoss/clj-symphony "0.1.0-SNAPSHOT"
+  :description         "A Clojure wrapper around the symphony-java-client library."
+  :url                 "https://github.com/symphonyoss/clj-symphony"
+  :license             {:name "Apache License, Version 2.0"
+                        :url  "http://www.apache.org/licenses/LICENSE-2.0"}
+  :min-lein-version    "2.5.0"
+  :repositories        [["sonatype-snapshots" {:url "https://oss.sonatype.org/content/groups/public" :snapshots true}]]
+  :plugins             [[lein-licenses "0.2.1"]]
+  :dependencies        [
+                         [org.clojure/clojure                      "1.8.0"]
+                         [org.symphonyoss.symphony/symphony-client "1.0.0-SNAPSHOT"]
+                       ]
+  :profiles            {:dev {:dependencies [[midje      "1.8.3"]]
+                              :plugins      [[lein-midje "3.2"]]}   ; Don't remove this or travis-ci will assplode!
+                        :uberjar {:aot :all}}
+  :deploy-repositories [
+                         ["snapshots" {:url "https://clojars.org/repo"
+                                       :username :env/clojars_username
+                                       :password :env/clojars_password}]
+                         ["releases"  {:url "https://clojars.org/repo"
+                                       :username :env/clojars_username
+                                       :password :env/clojars_password}]
+                       ]
+  :lein-release        {:deploy-via :clojars}
   )
